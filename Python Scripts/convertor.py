3881594a6da9299aa0095131a94d7ba0ac13d889
Leo Adlakha <leoa.co18@nsut.ac.in>
2020-12-09 11:38:08 +0530

    script ready convertor.py

---
+++ Python Scripts/convertor.py
@@ -0,0 +1,43 @@
+from __future__ import print_function
+from imutils.video import VideoStream
+import numpy as np
+import argparse
+import imutils
+import time
+import cv2 as cv
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-o", "--output", required=True, help="path to output video file")
+ap.add_argument("-f", "--fps", type=int, default=30, help="FPS of output video")
+ap.add_argument("-c", "--codec", type=str, default="h264", help="codec of output video")
+ap.add_argument("-i", "--input", required=True, help="path to input video file")
+args = vars(ap.parse_args())
+
+cap = cv.VideoCapture(args["input"])
+codec = cv.VideoWriter_fourcc(*args["codec"])
+writer = None
+(h, w) = (None, None)
+zeros = None
+
+while True :
+    ret, frame = cap.read()
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+    if writer is None:
+        (h, w) = frame.shape[:2]
+        writer = cv.VideoWriter(args["output"], codec, args["fps"], (w, h), True)
+        zeros = np.zeros((h, w), dtype="uint8")
+
+    # print(frame.shape)
+    
+    writer.write(frame)
+
+    # cv.imshow('frame', frame)
+
+    if cv.waitKey(1) == ord('q'):
+        break
+
+cap.release()
+writer.release()
+cv.destroyAllWindows()
