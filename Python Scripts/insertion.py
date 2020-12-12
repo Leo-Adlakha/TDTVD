80c47fbb7aa0b3de58bcf1c59c0a6a064b692a47
Leo Adlakha <leoa.co18@nsut.ac.in>
2020-12-12 10:55:22 +0530

    Recovered from Disaster ( Nothing Special )

--- Python Scripts/insertion.py
+++ Python Scripts/insertion.py
@@ -1,79 +1,91 @@
 import numpy as np
 import cv2 as cv
 
-cap = cv.VideoCapture("../original_video/example.mp4")
-cap2 = cv.VideoCapture("../original_video/example2.mp4")
-codec = cv.VideoWriter_fourcc(*"h264")
-writer = None
-(h, w) = (None, None)
-ins_start = 30
-ins_end = 80
-start_ins = 100
-count = 0
-storage = []
+txt_file = "/Users/leoadlakha/Documents/Research Work/TDTVD/csv/input.txt"
 
-while True :
-    
-    ret, frame = cap2.read() ;
-    if not ret :
-        print("Can't receive frame (stream end?). Exiting ...")
-        break
-        
-    count += 1
-    
-    if writer is None :
-        (h, w) = frame.shape[:2]
-    
-    if count >= ins_start and count <= ins_end :
-        storage.append(frame) 
-        
-    if cv.waitKey(1) == ord('q') :
-        break 
+with open(txt_file, "r") as my_input_file :
+    data = my_input_file.read().splitlines(True)
 
-writer = cv.VideoWriter("../test/output_insertion.mp4", codec, 30, (w, h), True)
-count = 0 
-while True :
-    
-    ret, frame = cap.read() ;
-    if not ret :
-        print("Can't receive frame (stream end?). Exiting ...")
-        break
-        
-    count += 1
-        
-    if cv.waitKey(1) == ord('q') :
-        break 
+for i in range(len(data)):
+    data[i] = data[i].rstrip('\n')
 
-total_frames = count
-count = 0 
-cap = cv.VideoCapture("../original_video/example.mp4")
+for i in range(len(data)) :
 
-while True :
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-    # if frame is read correctly ret is True
-    if not ret:
-        print("Can't receive frame (stream end?). Exiting ...")
-        break 
+    (insert_in, insert_from, start_ins, ins_start, ins_end) = data[i].split(" ")
+
+    cap = cv.VideoCapture("../original_video/example.mp4")
+    cap2 = cv.VideoCapture("../original_video/example2.mp4")
+    codec = cv.VideoWriter_fourcc(*"h264")
+    writer = None
+    (h, w) = (None, None)
+    ins_start = 30
+    ins_end = 80
+    start_ins = 100
+    count = 0
+    storage = []
+
+    while True :
         
-    count += 1
+        ret, frame = cap2.read()
+        if not ret :
+            print("Can't receive frame (stream end?). Exiting ...")
+            break
+            
+        count += 1
         
-    if count == start_ins :
-        for i in storage :
-            writer.write(i)
-            count += 1
-    
-    if count == total_frames + 1 :
-        break
-    
-    writer.write(frame)
-    
-    # Display the resulting frame
-    # cv.imshow('frame', frame)
-    if cv.waitKey(1) == ord('q'):
-        break
+        if writer is None :
+            (h, w) = frame.shape[:2]
         
-cap.release()
-cap2.release()
-writer.release()
-cv.destroyAllWindows()
+        if count >= ins_start and count <= ins_end :
+            storage.append(frame) 
+            
+        if cv.waitKey(1) == ord('q') :
+            break 
+
+    writer = cv.VideoWriter("../test/output_insertion.mp4", codec, 30, (w, h), True)
+    count = 0 
+    while True :
+        
+        ret, frame = cap.read() ;
+        if not ret :
+            print("Can't receive frame (stream end?). Exiting ...")
+            break
+            
+        count += 1
+            
+        if cv.waitKey(1) == ord('q') :
+            break 
+
+    total_frames = count
+    count = 0 
+    cap = cv.VideoCapture("../original_video/example.mp4")
+
+    while True :
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+        # if frame is read correctly ret is True
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting ...")
+            break 
+            
+        count += 1
+            
+        if count == start_ins :
+            for i in storage :
+                writer.write(i)
+                count += 1
+        
+        if count == total_frames + 1 :
+            break
+        
+        writer.write(frame)
+        
+        # Display the resulting frame
+        # cv.imshow('frame', frame)
+        if cv.waitKey(1) == ord('q'):
+            break
+            
+    cap.release()
+    cap2.release()
+    writer.release()
+    cv.destroyAllWindows()
